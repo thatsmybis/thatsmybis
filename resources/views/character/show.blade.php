008c91524ce083e448b6a0af7c17e9fb040ced7c
Lemmings19 <lemmings19@gmail.com>
2020-11-11 01:37:13 +0000

    Bugfix received item template using wrong received date.

--- resources/views/character/show.blade.php
+++ resources/views/character/show.blade.php
@@ -105,7 +105,7 @@
                         <ol class="">
                             @foreach ($character->received as $item)
                                 <li class="">
-                                    @include('partials/item', ['wowheadLink' => false, 'itemDate' => $item->pivot->created_at, 'itemUsername' => $item->added_by_username])
+                                    @include('partials/item', ['wowheadLink' => false, 'itemDate' => ($item ? ($item->pivot->received_at ? $item->pivot->received_at : $item->pivot->created_at) : null), 'itemUsername' => $item->added_by_username])
                                     @include('character/partials/itemDetails', ['hideCreatedAt' => true])
                                 </li>
                             @endforeach
