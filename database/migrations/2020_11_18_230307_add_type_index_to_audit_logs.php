f1e600f8593e34bc01b5250af9652a4a28020b3b
Lemmings19 <lemmings19@gmail.com>
2020-11-18 23:05:34 +0000

    Added index to audit log type field.

---
+++ database/migrations/2020_11_18_230307_add_type_index_to_audit_logs.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeindexToAuditLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('audit_logs', function (Blueprint $table) {
+            $table->index('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('audit_logs', function (Blueprint $table) {
+            //
+        });
+    }
+}
