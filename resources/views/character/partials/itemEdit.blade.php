e8122077e63e07eba323017289123763df310697
Lemmings19 <lemmings19@gmail.com>
2021-03-28 01:47:16 +0000

    Fixes and tweaks to item autocomplete and setting OS/Received flags on wishlist items.

--- resources/views/character/partials/itemEdit.blade.php
+++ resources/views/character/partials/itemEdit.blade.php
@@ -1,26 +1,30 @@
-@if ($item)
-    @php
+@php
+    if (isset($item) && $item) {
         $oldIsReceived = old($name . '.' . $i . '.is_received');
-        $oldIsOffspec = old($name . '.' . $i . '.is_offspec');
-    @endphp
-    <ul class="list-inline">
-        <li class="list-inline-item">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="{{ $name }}[{{ $index }}][is_received]" value="1" class="" autocomplete="off"
-                        {{ $oldIsReceived && $oldIsReceived == 1 ? 'checked' : ($item->pivot->is_received ? 'checked' : '') }}>
-                        Received
-                </label>
-            </div>
-        </li>
-        <li class="list-inline-item">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="{{ $name }}[{{ $index }}][is_offspec]" value="1" class="" autocomplete="off"
-                        {{ $oldIsOffspec && $oldIsOffspec == 1 ? 'checked' : ($item->pivot->is_offspec ? 'checked' : '') }}>
-                        OS
-                </label>
-            </div>
-        </li>
-    </ul>
-@endif
+        $oldIsOffspec  = old($name . '.' . $i . '.is_offspec');
+    } else {
+        $item          = null;
+        $oldIsReceived = false;
+        $oldIsOffspec  = false;
+    }
+@endphp
+<ul class="list-inline">
+    <li class="list-inline-item">
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="{{ $name }}[{{ $index }}][is_received]" value="1" class="" autocomplete="off"
+                    {{ $oldIsReceived && $oldIsReceived == 1 ? 'checked' : ($item && $item->pivot->is_received ? 'checked' : '') }}>
+                    Received
+            </label>
+        </div>
+    </li>
+    <li class="list-inline-item">
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="{{ $name }}[{{ $index }}][is_offspec]" value="1" class="" autocomplete="off"
+                    {{ $oldIsOffspec && $oldIsOffspec == 1 ? 'checked' : ($item && $item->pivot->is_offspec ? 'checked' : '') }}>
+                    OS
+            </label>
+        </div>
+    </li>
+</ul>
