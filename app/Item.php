634badf325359b7986eff97f47376a55105b8447
Lemmings19 <lemmings19@gmail.com>
2021-05-02 04:04:31 +0000

    Fixed item received characters bug.

--- app/Item.php
+++ app/Item.php
@@ -168,7 +168,7 @@ class Item extends Model
                 'characters.*',
                 'raid_groups.name AS raid_group_name',
                 'raid_group_roles.color AS raid_group_color',
-                DB::raw('MAX(added_by_members.username) AS added_by_username'),
+                'added_by_members.username AS added_by_username',
             ])
             ->whereNull('characters.inactive_at')
             ->leftJoin('raid_groups', function ($join) {
