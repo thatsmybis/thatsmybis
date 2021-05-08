15b021943a19225b074b6e4eb0fb063f305a36b7
Lemmings19 <lemmings19@gmail.com>
2021-05-08 06:22:36 +0000

    Hey look, I fixed the migration buggit diff! :rofl: ... will probably want to find an actual fix at some point.

--- database/migrations/2021_04_11_012812_change_raid_to_raid_groups_everywhere.php
+++ database/migrations/2021_04_11_012812_change_raid_to_raid_groups_everywhere.php
@@ -38,37 +38,37 @@ class ChangeRaidToRaidGroupsEverywhere extends Migration
         Schema::table('character_items', function (Blueprint $table) {
             $table->renameColumn('raid_id', 'raid_group_id');
             $table->foreign('raid_group_id')->references('id')->on('raid_groups');
-            $table->renameIndex('character_items_raid_id_foreign', 'character_items_raid_group_id_foreign');
+            // $table->renameIndex('character_items_raid_id_foreign', 'character_items_raid_group_id_foreign');
         });
         Schema::table('characters', function (Blueprint $table) {
             $table->renameColumn('raid_id', 'raid_group_id');
             $table->foreign('raid_group_id')->references('id')->on('raid_groups');
-            $table->renameIndex('characters_raid_id_foreign', 'characters_raid_group_id_foreign');
+            // $table->renameIndex('characters_raid_id_foreign', 'characters_raid_group_id_foreign');
         });
         Schema::table('content', function (Blueprint $table) {
             $table->renameColumn('raid_id', 'raid_group_id');
             $table->foreign('raid_group_id')->references('id')->on('raid_groups');
-            $table->renameIndex('content_raid_id_foreign', 'content_raid_group_id_foreign');
+            // $table->renameIndex('content_raid_id_foreign', 'content_raid_group_id_foreign');
         });
         Schema::table('batches', function (Blueprint $table) {
             $table->renameColumn('raid_id', 'raid_group_id');
             $table->foreign('raid_group_id')->references('id')->on('raid_groups');
-            $table->renameIndex('batches_raid_id_foreign', 'batches_raid_group_id_foreign');
+            // $table->renameIndex('batches_raid_id_foreign', 'batches_raid_group_id_foreign');
         });
         Schema::table('audit_logs', function (Blueprint $table) {
             $table->renameColumn('raid_id', 'raid_group_id');
             $table->foreign('raid_group_id')->references('id')->on('raid_groups');
-            $table->renameIndex('audit_logs_raid_id_foreign', 'audit_logs_raid_group_id_foreign');
+            // $table->renameIndex('audit_logs_raid_id_foreign', 'audit_logs_raid_group_id_foreign');
         });
 
         Schema::table('raid_groups', function (Blueprint $table) {
             $table->foreign('guild_id')->references('id')->on('guilds');
             $table->foreign('role_id')->references('id')->on('roles');
 
-            $table->renameIndex('raids_disabled_at_index', 'raid_groups_disabled_at_index');
-            $table->renameIndex('raids_guild_id_foreign', 'raid_groups_guild_id_foreign');
-            $table->renameIndex('raids_role_id_foreign', 'raid_groups_role_id_foreign');
-            $table->renameIndex('raids_slug_index', 'raid_groups_slug_index');
+            // $table->renameIndex('raids_disabled_at_index', 'raid_groups_disabled_at_index');
+            // $table->renameIndex('raids_guild_id_foreign', 'raid_groups_guild_id_foreign');
+            // $table->renameIndex('raids_role_id_foreign', 'raid_groups_role_id_foreign');
+            // $table->renameIndex('raids_slug_index', 'raid_groups_slug_index');
         });
     }
 
