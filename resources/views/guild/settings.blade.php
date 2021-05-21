68a69d654e144c386449549e687af9874446e942
Lemmings19 <lemmings19@gmail.com>
2021-05-21 06:12:15 +0000

    Added guild note for max wishlist items.

--- resources/views/guild/settings.blade.php
+++ resources/views/guild/settings.blade.php
@@ -263,6 +263,7 @@
                         <div class="form-group">
                             <label for="max_wishlist_items" class="">
                                 Max Wishlist Items
+                                <small class="text-muted">existing wishlists will need to be resubmitted</small>
                             </label>
                             <input name="max_wishlist_items" min="0" max="{{ App\Http\Controllers\CharacterController::MAX_WISHLIST_ITEMS }}" type="number" class="form-control dark" placeholder="{{ App\Http\Controllers\CharacterController::MAX_WISHLIST_ITEMS }}" value="{{ old('max_wishlist_items') ? old('max_wishlist_items') : $guild->max_wishlist_items }}" />
                         </div>
