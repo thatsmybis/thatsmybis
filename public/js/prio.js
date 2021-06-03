c5012b86d1ebf230d0adf87e9d8c48fe9cc9852d
Lemmings19 <lemmings19@gmail.com>
2021-06-03 06:34:15 +0000

    Updated prio custom JS.

---
+++ public/js/prio.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    warnBeforeLeaving("#editForm")
+
+    // sortable() is slow to initialize when applied to hundreds of elements, so this solves for that scenario
+    $(".js-sortable-lazy").one("mouseenter", function() {
+        $(this).sortable({
+            handle: ".js-sort-handle",
+            // Update the order palceholders once sorting finishes
+            stop: function(event, ui) {
+                updatePlaceholders();
+            },
+        });
+    });
+
+});
+
+// Update the placeholder values of anything with the given name.
+// Good for resetting a list's indexed placeholders after the list is reordered.
+function updatePlaceholders() {
+    $("ol.js-sortable-lazy").each(function () {
+        let index = 0;
+        $(this).find(`input[name$="[order]"]`).each(function () {
+            index++;
+            $(this).attr("placeholder", index);
+        });
+    });
+}
