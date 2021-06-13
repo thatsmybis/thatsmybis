6aa8855a5d1964b1bc287a31368ea5bd9fc64fd8
Lemmings19 <lemmings19@gmail.com>
2021-06-13 23:05:20 +0000

    Added DB index to user discord_username.

---
+++ database/migrations/2021_06_13_202432_add_indexes_for_admin_search.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexesForAdminSearch extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->index('discord_username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
