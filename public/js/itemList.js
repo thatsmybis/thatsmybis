5b79635fdb43dc0d61bb95a22805edf105f7f823
Lemmings19 <lemmings19@gmail.com>
2021-06-23 06:00:57 +0000

    Renamed +1 to 1r for raid count on item list page.

--- public/js/itemList.js
+++ public/js/itemList.js
@@ -232,7 +232,7 @@ function getCharacterList(data, type, itemId) {
                         <span class="text-warning">alt</span>
                     ` : '' }
                     ${ !guild.is_attendance_hidden && (character.attendance_percentage || character.raid_count) ?
-                        `${ character.raid_count && typeof character.attendance_percentage === 'number' ? `<span title="attendance" class="smaller ${ getAttendanceColor(character.attendance_percentage) }">${ Math.round(character.attendance_percentage * 100) }%</span>` : '' }${ character.raid_count ? `<span class="smaller">+${ character.raid_count }</span>` : ``}
+                        `${ character.raid_count && typeof character.attendance_percentage === 'number' ? `<span title="attendance" class="smaller ${ getAttendanceColor(character.attendance_percentage) }">${ Math.round(character.attendance_percentage * 100) }%</span>` : '' }${ character.raid_count ? `<span class="smaller"> ${ character.raid_count }r</span>` : ``}
                     ` : `` }
                     <span class="js-watchable-timestamp smaller"
                         data-timestamp="${ character.pivot.created_at }"
