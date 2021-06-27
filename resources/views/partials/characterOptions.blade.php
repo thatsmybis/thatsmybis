9d453c274fee06e962ded9a96f7fdcb3fb035647
Lemmings19 <lemmings19@gmail.com>
2021-06-27 09:26:18 +0000

    Style character dropdowns with class colors on selection.

--- resources/views/partials/characterOptions.blade.php
+++ resources/views/partials/characterOptions.blade.php
@@ -1,16 +1,20 @@
 @foreach ($characters as $character)
+    @php
+        $characterNameText = $character->name;
+        $characterMetaText = (isset($showClass) && $showClass ? ($character->class ? '(' . $character->class . ')&nbsp;' : '') : '');
+        $characterMetaText .= ($character->is_alt ? "Alt" : '');
+        if (isset($raidGroups) && $raidGroups->where('id', $character->raid_group_id)->count()) {
+            $characterMetaText .= '&sdot; ' . ($raidGroups->where('id', $character->raid_group_id)->first()->name);
+        }
+    @endphp
     <option value="{{ $character->id }}"
         data-tokens="{{ $character->id }}"
         data-raid-group-id="{{ $character->raid_group_id }}"
         data-name="{{ $character->name }}"
         data-class="text-{{ strtolower($character->class) }}"
         class="js-character-option text-{{ strtolower($character->class) }}-important"
+        data-content="<span class='font-weight-medium text-{{ strtolower($character->class) }}-important'>{{ $characterNameText }}</span> <span class='small'>{{ $characterMetaText }}</span>"
         hack="{{ $character->id }}">
-        {{ $character->name }}&nbsp;
-        {{ isset($showClass) && $showClass ? ($character->class ? '(' . $character->class . ')&nbsp;' : '') : '' }}
-        {{ $character->is_alt ? "Alt" : '' }}
-        @if (isset($raidGroups) && $raidGroups->where('id', $character->raid_group_id)->count())
-            &nbsp; ({{ $raidGroups->where('id', $character->raid_group_id)->first()->name }})
-        @endif
+        {{ $characterNameText }} {{ $characterMetaText }}
     </option>
 @endforeach
