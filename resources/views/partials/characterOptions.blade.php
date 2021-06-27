47114b41c36ebd7af96a386431ac3708d82ba049
Lemmings19 <lemmings19@gmail.com>
2021-06-27 09:27:26 +0000

    Style character dropdowns with class colors on selection.

--- resources/views/partials/characterOptions.blade.php
+++ resources/views/partials/characterOptions.blade.php
@@ -13,7 +13,7 @@
         data-name="{{ $character->name }}"
         data-class="text-{{ strtolower($character->class) }}"
         class="js-character-option text-{{ strtolower($character->class) }}-important"
-        data-content="<span class='font-weight-medium text-{{ strtolower($character->class) }}-important'>{{ $characterNameText }}</span> <span class='small'>{{ $characterMetaText }}</span>"
+        data-content="<span class='font-weight-medium text-{{ strtolower($character->class) }}-important'>{{ $characterNameText }}</span> <span class='small text-muted'>{{ $characterMetaText }}</span>"
         hack="{{ $character->id }}">
         {{ $characterNameText }} {{ $characterMetaText }}
     </option>
