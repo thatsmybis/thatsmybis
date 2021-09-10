fa287787fd003a5fc4888d45f4e81a610ae2d5f3
Lemmings19 <lemmings19@gmail.com>
2021-09-10 23:08:00 +0000

    Added quick link for Gargul export.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -411,15 +411,15 @@
                         <a class="dropdown-item" href="{{ route('guild.exports', ['guildId' => $guild->id, 'guildSlug' => $guild->slug]) }}">
                             {{ __("Exports") }}
                         </a>
-                        <a class="dropdown-item" target="_blank" href="{{ route('guild.export.addonItems', ['guildId' => $guild->id, 'guildSlug' => $guild->slug, 'fileType' => 'html']) }}">
-                            {{ __("Export for TMB Tooltips") }}
-                        </a>
                         <form role="form" method="POST" target="_blank" action="{{ route('guild.export.gargul', ['guildId' => $guild->id, 'guildSlug' => $guild->slug]) }}">
                             {{ csrf_field() }}
                             <button class="dropdown-item text-white" type="submit">
-                                {{ __("Export for Gargul") }}
+                                {{ __("Export Gargul") }}
                             </button>
                         </form>
+                        <a class="dropdown-item" target="_blank" href="{{ route('guild.export.addonItems', ['guildId' => $guild->id, 'guildSlug' => $guild->slug, 'fileType' => 'html']) }}">
+                            {{ __("Export TMB Tooltips") }}
+                        </a>
                         <a class="dropdown-item" href="{{ route('guild.members.list', ['guildId' => $guild->id, 'guildSlug' => $guild->slug]) }}">
                             {{ __("Members") }}
                         </a>
