2cb6aa66c81b06aefeea55bf085047b25d98a6b0
Lemmings19 <lemmings19@gmail.com>
2021-09-11 01:08:11 +0000

    Fix for member average attendance percentage?

--- resources/views/member/list.blade.php
+++ resources/views/member/list.blade.php
@@ -54,10 +54,10 @@
                                         $raidCount = $member->charactersWithAttendance->sum(function ($character) {
                                             return $character->raid_count;
                                         });
-                                        $attendancePercentage = $member->charactersWithAttendance->where('raid_count', '>', 0)->sum(function ($character) {
-                                            return $character->attendance_percentage;
+                                        $raidsAttended = $member->charactersWithAttendance->where('raid_count', '>', 0)->sum(function ($character) {
+                                            return $character->raid_count * $character->attendance_percentage;
                                         });
-                                        $attendancePercentage = $attendancePercentage ? ($attendancePercentage / $member->charactersWithAttendance->where('raid_count', '>', 0)->count()) : $attendancePercentage;
+                                        $attendancePercentage = $raidsAttended / $raidCount;
                                     @endphp
                                     @include('member/partials/listMember', ['raidCount' => $raidCount, 'attendancePercentage' => $attendancePercentage])
                                 </td>
