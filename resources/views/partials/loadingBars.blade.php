5435cc63bd75bb9725c9bedccf7fc13361aed90a
Lemmings19 <lemmings19@gmail.com>
2021-09-22 00:09:15 +0000

    Previous commit also had loading bar changes.

---
+++ resources/views/partials/loadingBars.blade.php
@@ -0,0 +1,12 @@
+<div class="loadingBarContainer d-flex justify-content-center">
+    <div class="flex-column">
+        <div class="loadingBars">
+            <div class="bg-expansion-{{ isset($guild->expansion_id) ? $guild->expansion_id : (isset($expansionId) ? $expansionId : null) }}"></div>
+            <div class="bg-expansion-{{ isset($guild->expansion_id) ? $guild->expansion_id : (isset($expansionId) ? $expansionId : null) }}"></div>
+            <div class="bg-expansion-{{ isset($guild->expansion_id) ? $guild->expansion_id : (isset($expansionId) ? $expansionId : null) }}"></div>
+        </div>
+        <div class="text-center text-expansion-{{ isset($guild->expansion_id) ? $guild->expansion_id : (isset($expansionId) ? $expansionId : null) }}">
+            {{ __("Loading...") }}
+        </div>
+    </div>
+</div>
