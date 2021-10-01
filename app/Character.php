103412a2ac96c497f7f0c6d831d8b7ed4bc8f4a5
Lemmings19 <lemmings19@gmail.com>
2021-10-01 22:04:40 +0000

    Removed unused alias.

--- app/Character.php
+++ app/Character.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use App\{BaseModel, Item, Guild, Member, Raid, RaidGroup};
-use Illuminate\Support\Facades\{DB, Route};
+use Illuminate\Support\Facades\{DB};
 use Illuminate\Database\Eloquent\Relations\{HasMany};
 
 class Character extends BaseModel
