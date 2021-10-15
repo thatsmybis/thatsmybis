01345f7c363fa8810f0b2ac71f31c84158b2d086
Lemmings19 <lemmings19@gmail.com>
2021-10-15 08:10:49 +0000

    WIP roster stats page.

---
+++ resources/views/partials/characterStatsDatatable.blade.php
@@ -0,0 +1,187 @@
+@include('partials/loadingBars')
+<div class="pr-2 pl-2" style="display:none;" id="characterStatsDatatable">
+    <ul class="list-inline mb-0">
+        <li class="list-inline-item">
+            <label for="raid_group_filter" class="font-weight-light">
+                <span class="text-muted fas fa-fw fa-helmet-battle"></span>
+                {{ __("Raid Group") }}
+            </label>
+            <select id="raid_group_filter" class="form-control dark selectpicker">
+                <option value="">—</option>
+                @foreach ($raidGroups->whereNull('disabled_at') as $raidGroup)
+                    <option value="{{ $raidGroup->id }}" style="color:{{ $raidGroup->getColor() }};">
+                        {{ $raidGroup->name }}
+                    </option>
+                @endforeach
+            </select>
+        </li>
+        <li class=" list-inline-item">
+            <label for="class_filter" class="font-weight-light">
+                <span class="text-muted fas fa-fw fa-axe-battle"></span>
+                {{ __("Class") }}
+            </label>
+            <select id="class_filter" class="form-control dark selectpicker">
+                <option value="">—</option>
+                @foreach (App\Character::classes($guild->expansion_id) as $key => $class)
+                    <option value="{{ $key }}" class="text-{{ strtolower($key) }}-important">
+                        {{ $class }}
+                    </option>
+                @endforeach
+            </select>
+        </li>
+        <li class="list-inline-item">
+            <label for="instance_filter" class="font-weight-light">
+                <span class="text-muted fas fa-fw fa-sack"></span>
+                {{ __("Dungeon") }}
+            </label>
+            <select id="instance_filter"
+                multiple
+                class="form-control dark selectpicker"
+                data-actions-box="true"
+                data-none-selected-text="—"
+                data-deselect-all-text="{{ __('Reset') }}"
+                data-select-all-text="{{ __('All') }}">
+                @if ($guild->expansion_id == 1)
+                    <option value="4">
+                        {{ __("Zul'Gurub") }}
+                    </option>
+                    <option value="5">
+                        {{ __("Ruins of Ahn'Qiraj") }}
+                    </option>
+                    <option value="8">
+                        {{ __("World Bosses") }}
+                    </option>
+                    <option value="1">
+                        {{ __("Molten Core") }}
+                    </option>
+                    <option value="2">
+                        {{ __("Onyxia's Lair") }}
+                    </option>
+                    <option value="3">
+                        {{ __("Blackwing Lair") }}
+                    </option>
+                    <option value="6">
+                        {{ __("Temple of Ahn'Qiraj") }}
+                    </option>
+                    <option value="7">
+                        {{ __("Naxxramas") }}
+                    </option>
+                @elseif ($guild->expansion_id == 2)
+                    <option value="9">
+                        {{ __("Karazhan") }}
+                    </option>
+                    <option value="10">
+                        {{ __("Gruul's Lair") }}
+                    </option>
+                    <option value="11">
+                        {{ __("Magtheridon's Lair") }}
+                    </option>
+                    <option value="12">
+                        {{ __("Serpentshrine Cavern") }}
+                    </option>
+                    <option value="13">
+                        {{ __("Hyjal Summit") }}
+                    </option>
+                    <option value="14">
+                        {{ __("Tempest Keep") }}
+                    </option>
+                    <option value="15">
+                        {{ __("Black Temple") }}
+                    </option>
+                    <option value="16">
+                        {{ __("Zul'Aman") }}
+                    </option>
+                    <option value="17">
+                        {{ __("Sunwell Plateau") }}
+                    </option>
+                    <option value="18">
+                        {{ __("World Bosses") }}
+                    </option>
+                @endif
+            </select>
+        </li>
+        <li class="list-inline-item">
+            @php
+                $wishlistNames = $guild->getWishlistNames();
+            @endphp
+            <label for="wishlist_filter" class="font-weight-light">
+                <span class="text-muted fas fa-fw fa-scroll-old"></span>
+                {{ __("Wishlist") }}
+            </label>
+            <select id="wishlist_filter" class="form-control dark selectpicker">
+                @for ($i = 1; $i <= App\Http\Controllers\CharacterLootController::MAX_WISHLIST_LISTS; $i++)
+                    <option value="{{ $i }}" {{ $guild->current_wishlist_number === $i ? 'selected' : '' }}>
+                        @if ($wishlistNames && $wishlistNames[$i - 1])
+                            {{ $wishlistNames[$i - 1] }}{{ $guild->current_wishlist_number === $i ? '*' : '' }}
+                        @else
+                            {{ $i }}{{ $guild->current_wishlist_number === $i ? '*' : '' }}
+                        @endif
+                    </option>
+                @endfor
+                <option value="">
+                    {{ __("All") }}
+                </option>
+            </select>
+        </li>
+    </ul>
+    <ul class="list-inline mb-0 mt-3">
+        @if ($showPrios)
+            <li class="list-inline-item">
+                <span class="toggle-column text-link cursor-pointer font-weight-light" data-column="1">
+                    <span class="text-muted fal fa-fw fa-sort-amount-down"></span>
+                    {{ __("Prios") }}
+                </span>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+        @endif
+        @if ($showWishlist)
+            <li class="list-inline-item">
+                <span class="toggle-column text-link cursor-pointer font-weight-light" data-column="2">
+                    <span class="text-muted fal fa-fw fa-scroll-old"></span>
+                    {{ __("Wishlist") }}
+                </span>
+            </li>
+            <li class="list-inline-item">&sdot;</li>
+        @endif
+        <li class="list-inline-item">
+            <span class="toggle-column text-link cursor-pointer font-weight-light" data-column="3">
+                <span class="text-muted fal fa-fw fa-sack"></span>
+                {{ __("Received") }}
+            </span>
+        </li>
+        <li class="list-inline-item">&sdot;</li>
+        <li class="list-inline-item">
+            <span class="toggle-column text-link cursor-pointer font-weight-light" data-column="6">
+                <span class="text-muted fal fa-fw fa-comment-alt-lines"></span>
+                {{ __("Notes") }}
+            </span>
+        </li>
+        <li class="list-inline-item">&sdot;</li>
+        <li class="list-inline-item">
+            <span class="js-hide-strikethrough-items text-link cursor-pointer font-weight-light" data-column="6">
+                <span class="text-muted fal fa-fw fa-strikethrough"></span>
+                {{ __("Hide") }}
+                <span class="font-strikethrough">{{ __("received") }}</span>
+            </span>
+        </li>
+        <li class="list-inline-item">&sdot;</li>
+        <li class="list-inline-item">
+            <span class="js-hide-offspec-items text-link cursor-pointer font-weight-light" data-column="6">
+                <span class="text-muted fal fa-fw fa-trash"></span>
+                {{ __("Hide OS") }}
+            </span>
+        </li>
+        <li class="list-inline-item">&sdot;</li>
+        <li class="list-inline-item">
+            <span class="js-show-all-clipped-items text-link cursor-pointer font-weight-light" data-column="6">
+                <span class="text-muted fal fa-fw fa-eye"></span>
+                {{ __("Show all loot") }}
+            </span>
+        </li>
+    </ul>
+</div>
+
+<div class="col-12 pb-3 pr-2 pl-2 rounded">
+    <table id="characterStatsTable" class="table table-border table-hover stripe">
+    </table>
+</div>
---
+++ resources/views/rosterStats.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+@section('title',  __("Roster") . ' - ' . config('app.name'))
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="row pt-2 mb-3">
+        <div class="col-12 pr-0 pl-0">
+            @include('partials/characterStatsDatatable')
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('scripts')
+<script>
+    var characters       = {!! $characters->makeVisible('officer_note')->toJson() !!};
+    var currentWishlistNumber = {{ $guild->current_wishlist_number }};
+    var guild            = {!! $guild->toJson() !!};
+    var instances        = {!! $instances->toJson() !!};
+    var maxWishlistLists = {{ App\Http\Controllers\CharacterLootController::MAX_WISHLIST_LISTS }};
+    var raidGroups       = {!! $raidGroups->toJson() !!};
+    var showEdit         = {{ $showEdit ? 'true' : 'false' }};
+    var showOfficerNote  = {{ $showOfficerNote ? 'true' : 'false' }};
+    var showPrios        = {{ $showPrios ? 'true' : 'false' }};
+    var showWishlist     = {{ $showWishlist ? 'true' : 'false' }};
+    var wishlistNames    = {!! $guild->getWishlistNames() ? json_encode($guild->getWishlistNames()) : 'null' !!};
+</script>
+<script src="{{ loadScript('rosterStats.js') }}"></script>
+@endsection
