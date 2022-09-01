e37810090a6b7d84ca06630eec980b48df7206f7
Lemmings19 <lemmings19@gmail.com>
2022-09-01 20:26:22 +0000

    Add DK to TBC.

--- app/Character.php
+++ app/Character.php
@@ -649,6 +649,7 @@ class Character extends BaseModel
                 break;
             case 2: // TBC
                 return [
+                    self::CLASS_DEATH_KNIGHT => __('Death Knight'),
                     self::CLASS_DRUID   => __('Druid'),
                     self::CLASS_HUNTER  => __('Hunter'),
                     self::CLASS_MAGE    => __('Mage'),
