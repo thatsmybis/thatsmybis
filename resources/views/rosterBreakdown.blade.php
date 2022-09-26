69f51cd31310b656ccc1de0820088a4e66f28c78
Lemmings19 <lemmings19@gmail.com>
2022-09-26 18:58:05 +0000

    Updated roster breakdown icon.

--- resources/views/rosterBreakdown.blade.php
+++ resources/views/rosterBreakdown.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-12 text-center pr-0 pl-0">
             <h1 class="font-weight-medium mb-0 font-blizz">
-                <span class="fas fa-fw fa-book text-gold"></span>
+                <span class="fas fa-fw fa-users-class text-success"></span>
                 {{ __("Roster Breakdown") }}
             </h1>
         </div>
