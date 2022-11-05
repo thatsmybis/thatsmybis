1724e07ac199843ac59067c40764427a2e985fd1
Lemmings19 <lemmings19@gmail.com>
2022-11-05 23:36:47 +0000

    Better offspec detection in loot import.

--- public/js/assignLoot.js
+++ public/js/assignLoot.js
@@ -508,7 +508,7 @@ function loadItemToForm(item, i) {
     if ((item['offspec'] && item['offspec'] == 1)) {
         offspec = 1;
     } else if (item['offspec'] == undefined || (item['offspec'] !== 0 && item['offspec'].toLowerCase() !== 'false' && item['offspec'].toLowerCase() !== 'no')) {
-        if (item['note'] && offspecFlags.includes(item['note'].toLowerCase())) {
+        if (item['note'] && (offspecFlags.includes(item['note'].toLowerCase()) || (publicNote && publicNote.toLowerCase().includes('offspec')) || (officerNote && officerNote.toLowerCase().includes('offspec')))) {
             offspec = 1;
         } else if (item['response'] && offspecFlags.includes(item['response'].toLowerCase())) {
             offspec = 1;
