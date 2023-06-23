58d10eb9b928c85b8a9eb8865f6824e80a6c5bda
Lemmings19 <lemmings19@gmail.com>
2023-06-23 01:40:58 +0000

    Fix bug where prio page would attempt to load wishlist numbers for guilds that have wishlists disabled... would query entire database.

--- resources/views/guild/prios/assignPrios.blade.php
+++ resources/views/guild/prios/assignPrios.blade.php
@@ -13,8 +13,8 @@
 
     foreach ($instance->itemSources as $itemSource) {
         $itemSourceAmounts[$itemSource->name] = [
-            'wishlistCount' => 0, //array_sum(array_keys($items->where('source_name', $itemSource->name)->countBy(function ($source) { return $source->wishlistCharacters->where('pivot.is_received', 0)->count();})->toArray())),
-            'prioCount'     => 0, //array_sum(array_keys($items->where('source_name', $itemSource->name)->countBy(function ($source) { return $source->priodCharacters->where('pivot.is_received', 0)->count();})->toArray())),
+            'wishlistCount' => $guild->is_wishlist_disabled ? 0 : array_sum(array_keys($items->where('source_name', $itemSource->name)->countBy(function ($source) { return $source->wishlistCharacters->where('pivot.is_received', 0)->count();})->toArray())),
+            'prioCount'     => $guild->is_prio_disabled ? 0 : array_sum(array_keys($items->where('source_name', $itemSource->name)->countBy(function ($source) { return $source->priodCharacters->where('pivot.is_received', 0)->count();})->toArray())),
         ];
     }
 @endphp
@@ -38,9 +38,6 @@
 
             <div class="row">
                 <div class="col-12 pt-2 mb-2">
-                    <div class="text-warning">
-                        Wishlist/prio summary counts are unavailable. I am fixing a bug with them.
-                    </div>
                     <ul class="list-inline mb-0">
                         @foreach ($instance->itemSources as $itemSource)
                             @if (!$loop->first)
