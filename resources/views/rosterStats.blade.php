d988123099c1cd2b9b2ab3d6c1febdde43f6e14f
Lemmings19 <lemmings19@gmail.com>
2024-04-20 08:05:04 +0000

    Add received loot filter to roster stats page.

--- resources/views/rosterStats.blade.php
+++ resources/views/rosterStats.blade.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
     <div class="row pt-2 mb-3">
         <div class="col-12 pr-0 pl-0">
-            @include('partials/characterStatsDatatable')
+            @include('partials/characterStatsDatatable', ['receivedLootDateFilter' => true])
         </div>
     </div>
 </div>
